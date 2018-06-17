fabf8e9ec40e6a670292e6c885b9a1397cffae6c
Curtis Ho <curtisho0718@hotmail.com>
2018-06-16 23:01:14 -0700

    update info

---
+++ public/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <p>&copy; all right reserved to Curtis Ho 2018</p>
+    </footer>
+
+</body>
+
+</html>
---
+++ public/menu.php
@@ -0,0 +1,25 @@
+<nav>
+<a href="#" class="logo">
+    <img src="img/logo.png" height="70px">
+</a>
+<a class="toggle">
+    <div class="menu icon"></div>
+    <div class="menu icon"></div>
+    <div class="menu icon"></div>
+</a>
+
+<ul class="">
+    <li>
+        <a href="index.html">Home</a>
+    </li>
+    <li>
+        <a href="#">Our Team</a>
+    </li>
+    <li>
+        <a href="sponsor.html">Partners</a>
+    </li>
+    <li>
+        <a href="#">Event Pictures</a>
+    </li>
+</ul>
+</nav>
